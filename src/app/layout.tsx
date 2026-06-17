bf62cf993ff82fffa39efd13d77e2bfa55d11b1f
lxcario <resquedzn05@gmail.com>
2026-06-17 03:17:52 +0300

    fix: remove 125% font-size, back to native 100%

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,6 @@ export default function RootLayout({
     <html
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-      style={{ fontSize: "125%" }}
     >
       <head>
         <link rel="preload" href="/sprites/ui/buttons-26x26.png" as="image" type="image/png" />
